ee19d592cf420ec3a5c9868bd7f60fd044d0dead
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2024-05-26 05:11:38 +0530

    user get

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -20,6 +20,21 @@ interface LoginRequestBody {
   password: string;
 }
 
+router.get(
+  "/users",
+  async (req: Request, res: Response) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+  }
+);
+
 router.post(
   "/signup",
   [
