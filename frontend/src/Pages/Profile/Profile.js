4f07657334baa1f661d9b255164af31621e763ab
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2024-05-26 14:57:02 +0530

    profile get

--- frontend/src/Pages/Profile/Profile.js
+++ frontend/src/Pages/Profile/Profile.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MailIcon, UserCircleIcon, TrashIcon, CurrencyDollarIcon, ShoppingCartIcon, GiftIcon } from '@heroicons/react/solid';
 import profileImage from '../Profile/jr.png';
-
+import { Link } from 'react-router-dom';
 const Profile = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/users/6652f06802fa2bc5f91cc3b5');
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <div className="bg-black min-h-screen pt-16 px-6 md:px-25 mt-10">
       <div className="container mx-auto p-8">
@@ -13,8 +29,8 @@ const Profile = () => {
               <img src={profileImage} alt="Profile" className="w-32 h-32" />
             </div>
             <div className="text-white">
-              <p className="text-lg">Name: Jigyasa</p>
-              <p className="text-lg">User ID: Matrix01_Glitch</p>
+              <p className="text-lg">Name: {userData ? userData.fullname : 'Loading...'}</p>
+              <p className="text-lg">User ID: {userData ? userData.username : 'Loading...'}</p>
             </div>
           </div>
         </div>
@@ -23,7 +39,7 @@ const Profile = () => {
           <div>
             <div className="flex items-center bg-opacity-30 bg-blue-900 border border-white p-4 rounded-lg hover:bg-opacity-50 transition duration-300">
               <CurrencyDollarIcon className="h-6 w-6 mr-2 text-white" />
-              <p className="text-white flex-grow">Wallet</p>
+              <Link to='/wallet'><p className="text-white flex-grow">Wallet</p></Link>
             </div>
             <div className="flex items-center bg-opacity-30 bg-blue-900 border border-white p-4 rounded-lg mt-4 hover:bg-opacity-50 transition duration-300">
               <ShoppingCartIcon className="h-6 w-6 mr-2 text-white" />
